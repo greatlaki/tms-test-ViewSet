21b475f463d3e3e9b847accffb786aa10f0e24ce
greatlaki <greatlaki911@gmail.com>
2022-09-28 18:16:34 +0300

    ViewSet
    We use the base class view sets. ViewSet we will inherit our ArticleView class from it and define methods
    for getting all articles (list) and one selected article (retrieve).
    Next, we need to link our list and retrieve methods to the API URL.

--- test_django/article/views.py
+++ test_django/article/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Article
+from .serializers import ArticleSerializer
 
-# Create your views here.
+
+class ArticleView(viewsets.ViewSet):
+    """
+    A simple ViewSet that for listing or retrieving users.
+    """
+    def list(self, request):
+        queryset = Article.objects.all()
+        serializer = ArticleSerializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    def retrieve(self, request, pk=None):
+        queryset = Article.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        serializer = ArticleSerializer(user)
+        return Response(serializer.data)
